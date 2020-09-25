d0b65dbd762c180d42e13c309efcd5808527da0c
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-09-24 18:41:50 -0700

    [stock_bot] Fix stock bot

--- stock_bot.py
+++ stock_bot.py
@@ -22,7 +22,6 @@ if 'Sign in here, please.' in driver.page_source:
 
 login_password = driver.find_element_by_xpath('//*[@id="user_password"]')
 signin_button = driver.find_element_by_xpath('//*[@id="new_user"]/div[5]/div[2]/div[1]/button')
-
 login_email.send_keys(email)
 login_password.send_keys(password)
 signin_button.click()
@@ -44,86 +43,36 @@ if 'Free Stock Screener' in driver.page_source:
 
 stock_list = stock_data.split('\n')
 
-stock_list[28].split(' $')
-company_info_parsed = stock_list[28].split(' $')[0]
-market_cap = stock_list[28].split(' $')[1]
-market_info = stock_list[28].split(' $')[2]
-price_info = stock_list[28].split(' $')[3].split(' ')
-price = price_info[0]
-performance_today = price_info[1]
-company_info = company_info_parsed.split(' ')
-if 'NASDAQ' in company_info:
-    idx = company_info.index('NASDAQ')
-    exchange = company_info[idx]
-elif 'NYSE'in company_info:
-    idx = company_info.index('NYSE')
-    exchange = company_info[idx]
-elif 'NYSEMKT' in company_info:
-    idx = company_info.index('NYSEMKT')
-    exchange = company_info[idx]
-industry = " ".join(company_info[idx+1:])
-symbol = company_info[0]
-company_name = " ".join(company_info[1:idx])
-company_name
-# currently parsed:
-# symbol, company_name, exchange, industry, market_cap, price, performance_today
-# ebitda, p_e, ev_ebitda, debt_equity, average_volume, institutional_ownership, earnings_date
 
-# parse market info
-ebitda = market_info.split(' ')[0]
-p_e = market_info.split(' ')[1]
-ev_ebitda = market_info.split(' ')[2]
-debt_equity = market_info.split(' ')[3]
-average_volume = market_info.split(' ')[4]
-institutional_ownership = market_info.split(' ')[5]
-earnings_date = " ".join(market_info.split(' ')[6:])
-earnings_date
 
-x_stock = {
-    "symbol": symbol,
-    "company_name": company_name,
-    "exchange": exchange,
-    "industry": industry,
-    "market_cap": market_cap,
-    "ebitda": ebitda,
-    "p_e": p_e,
-    "ev_ebitda": ev_ebitda,
-    "debt_equity": debt_equity,
-    "average_volume": average_volume,
-    "institutional_ownership": institutional_ownership,
-    "earnings_date": earnings_date,
-    "price": price,
-    "performance_today": performance_today
-}
+# all_stocks = stock_list[28:]
 
-all_stocks = stock_list[28:]
+# index_list = []
+# final_list = []
 
-index_list = []
-final_list = []
-
-for i in range(len(all_stocks)):
-    each_stock = all_stocks[i]
+# for i in range(len(all_stocks)):
+#     each_stock = all_stocks[i]
     
-    if 'NT' in each_stock:
-        pass
-    elif '¥' in each_stock:
-        pass
-    elif '€' in each_stock:
-        pass
-    elif 'kr' in each_stock:
-        pass
-    else:
-        final_list.append(each_stock)
+#     if 'NT' in each_stock:
+#         pass
+#     elif '¥' in each_stock:
+#         pass
+#     elif '€' in each_stock:
+#         pass
+#     elif 'kr' in each_stock:
+#         pass
+#     else:
+#         final_list.append(each_stock)
 
-driver.get(f"https://wallmine.com/{x.get('exchange')}/{x.get('symbol')}")
+# driver.get(f"https://wallmine.com/{x.get('exchange')}/{x.get('symbol')}")
 
-for each_stock in final_list:
-    x = parse_stock_data(each_stock)
-    driver.get(f"https://wallmine.com/{x.get('exchange')}/{x.get('symbol')}")
-    time.sleep(3)
+# for each_stock in final_list:
+#     x = parse_stock_data(each_stock)
+#     driver.get(f"https://wallmine.com/{x.get('exchange')}/{x.get('symbol')}")
+#     time.sleep(3)
     
-    if x.get('symbol') in driver.page_source:
-        print(f"We are on {x.get('company_name')}'s stock page")
+#     if x.get('symbol') in driver.page_source:
+#         print(f"We are on {x.get('company_name')}'s stock page")
 
 
 def parse_stock_data(data):
@@ -205,6 +154,17 @@ for i in range(len(stock_list[28:])):
         final_list.append(each_stock)
 
 
+# for each_stock in final_list:
+#     print(parse_stock_data(each_stock))
+#     time.sleep(1)
+
 for each_stock in final_list:
-    print(parse_stock_data(each_stock))
-    time.sleep(1)
+    x = parse_stock_data(each_stock)
+    driver.get(f"https://wallmine.com/{x.get('exchange')}/{x.get('symbol')}")
+    if x.get('symbol') in driver.page_source:
+        print(f"We are on {x.get('company_name')} stock page")
+        print('---')
+    print(x)
+    print('==================================')
+    time.sleep(2)
+driver.quit()
